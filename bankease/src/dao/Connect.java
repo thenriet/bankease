fe739f3153ef5bff1e04a57cac6265e0b59f513d
thenriet <dabalx@gmail.com>
2023-01-19 10:45:55 +0100

    git ignored updated

---
+++ bankease/src/dao/Connect.java
@@ -0,0 +1,20 @@
+
+package dao;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Connect {
+	
+	private static final String host = "jdbc:mysql://localhost:";
+	private static final String port = "3306";
+	private static final String db = "/gestion_database";
+	
+	private static final String user = "thenriet";
+	private static final String password = "thomas1991";
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(host + port + db, user,
+				password);
+		}
+}
