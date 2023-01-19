bf8f4d634c506c2dc9ab2024a9ff34114f723360
thenriet <dabalx@gmail.com>
2023-01-19 09:59:03 +0100

    added main back

---
+++ bankease/src/bankease/Main.java
@@ -0,0 +1,17 @@
+package bankease;
+import dao.CheckingAccountDAO;
+import view.CheckingAccountView;
+import view.ClientsList;
+import view.FrmAccountList;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//FrmAccountList mainFrame = new FrmAccountList(4);
+		//CheckingAccountView a = new CheckingAccountView();
+		//a.setVisible(true);
+		new ClientsList();
+
+	}
+}
