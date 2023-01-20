0d5e79a3fe6ebea4de5864609fb47d05e1aff93b
MyleneCaudron <milenecaudron@gmail.com>
2023-01-20 09:35:27 +0100

    ouvrircompte2

--- bankease/src/bankease/Main.java
+++ bankease/src/bankease/Main.java
@@ -3,15 +3,19 @@ import dao.CheckingAccountDAO;
 import view.CheckingAccountView;
 import view.ClientsList;
 import view.FrmAccountList;
+import view.OuvrirCompte2;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		//FrmAccountList mainFrame = new FrmAccountList(4);
-		//CheckingAccountView a = new CheckingAccountView();
-		//a.setVisible(true);
-		new ClientsList();
-
+//		CheckingAccountView a = new CheckingAccountView();
+//		a.setVisible(true);
+		//new ClientsList();
+		//new FrmAccountList(4);
+		OuvrirCompte2 a = new OuvrirCompte2();
+		a.setVisible(true);
+		
 	}
 }
