f5a35da69937d040cad0bc6e3c796df5ef3e462e
Thomas Henriet <dabalx@gmail.com>
2023-01-22 11:23:01 +0100

    merge with pooling and now dev

--- bankease/src/dao/Connect.java
+++ bankease/src/dao/Connect.java
@@ -10,8 +10,8 @@ public class Connect {
 	private static final String host = "jdbc:mysql://localhost:";
 	private static final String port = "3306";
 	private static final String db = "/gestion_database";
-	private static final String user = "slebrun";
-	private static final String password = "Simplon59";
+	private static final String user = "thenriet";
+	private static final String password = "thomas1991";
 	
 	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(host + port + db, user,
